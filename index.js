1d6297e7bb369361cc710e40a1564792059623f0
Zenab Waqi <zeevaki@gmail.com>
2026-04-23 17:56:20 -0500

    Improved Banner and Logo

--- index.js
+++ index.js
@@ -7,21 +7,3 @@ function closeMenu() {
     document.body.classList.remove("menu--open");
 }
 
-import Testimonials, { Testimonials } from './Testimonials';
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-// 1. ADD THIS LINE RIGHT HERE:
-import Testimonials from './Testimonials'; 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    
-    {/* 2. DROP YOUR NEW COMPONENT IN HERE: */}
-    <Testimonials />
-
-  </React.StrictMode>
-);
-// This line allows other files to use this component
-export default Testimonials;
